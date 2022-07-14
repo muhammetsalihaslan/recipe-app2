0237934997a251105b8dd3f6df8389311b679128
salih <muhammetsalihaslan@gmail.com>
2022-07-14 17:51:24 +0300

    continue

--- recipe-app/src/component/nav/Navbar.jsx
+++ recipe-app/src/component/nav/Navbar.jsx
@@ -1,8 +1,19 @@
 import NavStyle from './Navbar.style';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav>
+        <div>
+            <i> {'<MSA/>'} </i>
+            <span>Recipe</span>
+        </div>
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="about">About</Link>
+            <Link to="register">Register</Link>
+        </div>
+        </nav>
     )
 };
 
--- recipe-app/src/router/PrivateRouter.jsx
+++ recipe-app/src/router/PrivateRouter.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 
 const PrivateRouter = () => {
-    const user = true;
+    const user = JSON.parse(sessionStorage.getItem('user'));
     return user ? <Outlet/> : <Navigate to="/login"/>;
 };
 
