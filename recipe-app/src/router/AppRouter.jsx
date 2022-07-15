18cf8e636b4b86aa263772b11796ebeface95c2b
salih <muhammetsalihaslan@gmail.com>
2022-07-15 11:43:48 +0300

    home

--- recipe-app/src/router/AppRouter.jsx
+++ recipe-app/src/router/AppRouter.jsx
@@ -5,6 +5,7 @@ import About from '../pages/about/About';
 import Detail from '../pages/detail/Detail';
 import Home from '../pages/home/Home';
 import Login from '../pages/login/Login';
+import Register from '../pages/register/Register';
 import PrivateRouter from './PrivateRouter';
 
 const AppRouter = () => {
@@ -16,6 +17,7 @@ const AppRouter = () => {
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login/>}/>
+        <Route path="register" element={<Register />} />
         <Route path="about" element={<PrivateRouter/>}>
             <Route path="" element={<About/>}/>
         </Route>
