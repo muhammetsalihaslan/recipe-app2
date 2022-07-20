deaa633a4700aaff01b078312c29ed1cc8ec81f4
salih <muhammetsalihaslan@gmail.com>
2022-07-20 14:23:08 +0300

    card

--- recipe-app/src/component/cards/Cards.jsx
+++ recipe-app/src/component/cards/Cards.jsx
@@ -1,9 +1,21 @@
-import CardStyle from './Cards.style';
+import { Button, Card, Header, Image, MainContainer } from './Cards.style';
+import defaultImage from '../../assets/default-image.jpg';
 
-const Cards = () => {
-    return (
-        <div>Cards</div>
-    )
+
+const Cards = ({recipes}) => {
+    return <MainContainer wrap="wrap">
+    {recipes.map(({recipe}, index)=>(
+    <Card key={index}>
+    <Header>{recipe.label}</Header>
+    <Image src={recipe.image || defaultImage }/>
+    <Button>View More</Button>
+
+    </Card>
+    ))}
+    </MainContainer>
+
+    
+    
 }
 
 export default Cards;
